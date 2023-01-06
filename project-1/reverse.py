09cca3593746c5fd0c86506a855318dbf17d168b
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-06 12:37:32 -0500

    complete code for reverse.py

--- project-1/reverse.py
+++ project-1/reverse.py
@@ -8,3 +8,18 @@ Created on Tue Jan  3 18:39:48 2023
 Date: 01/03/2023
 """
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+reverse_alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+
+encrypted_message = input("Enter the encrypted message: ")
+
+decrypted_message = ""
+
+for letter in encrypted_message:
+    if letter in alphabet:
+        index = alphabet.index(letter)
+        decrypted_message += reverse_alphabet[index]
+    else:
+        decrypted_message += letter
+
+print("The plaintext message is:", decrypted_message)
