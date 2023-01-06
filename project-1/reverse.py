742ee896604cc1495e733bd863e746e6a16d7b58
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-06 13:29:55 -0500

    added comments to reverse.py

--- project-1/reverse.py
+++ project-1/reverse.py
@@ -7,12 +7,13 @@ Created on Tue Jan  3 18:39:48 2023
 @JHED: pkariuk1
 Date: 01/03/2023
 """
-
+# Normal list of the alphabet
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# List of the alphabet in reverse order
 reverse_alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
 
 encrypted_message = input("Enter the encrypted message: ")
-
+# String representing the decrypted message
 decrypted_message = ""
 
 for letter in encrypted_message:
@@ -20,6 +21,7 @@ for letter in encrypted_message:
         index = alphabet.index(letter)
         decrypted_message += reverse_alphabet[index]
     else:
+        # Caters for the space characters in encrypted message
         decrypted_message += letter
 
 print("The plaintext message is:", decrypted_message)
