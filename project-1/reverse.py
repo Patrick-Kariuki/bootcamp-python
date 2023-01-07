8e43c1e5c29af613fcef3b8ed50310f72b61c0f6
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-06 23:26:12 -0500

    updated doc string with project description

--- project-1/reverse.py
+++ project-1/reverse.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  3 18:39:48 2023
-
 @author: Patrick Kariuki
 @JHED: pkariuk1
 Date: 01/03/2023
+
+Bootcamp Python
+Johns Hopkins University
+Intersession 2023
+
+Project 1A: Decrypting encrypted message using reverse alphabet.
 """
 # Normal list of the alphabet
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # List of the alphabet in reverse order
-reverse_alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+reverse_alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p',
+                    'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e',
+                    'd', 'c', 'b', 'a']
 
 encrypted_message = input("Enter the encrypted message: ")
 # String representing the decrypted message
@@ -21,7 +28,7 @@ for letter in encrypted_message:
         index = alphabet.index(letter)
         decrypted_message += reverse_alphabet[index]
     else:
-        # Caters for the space characters in encrypted message
+        # Caters for punctuation and whitespace in encrypted message
         decrypted_message += letter
 
 print("The plaintext message is:", decrypted_message)
--- project-1/shifted.py
+++ project-1/shifted.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  3 18:40:36 2023
-
 @author: Patrick Kariuki
 @JHED: pkariuk1
 Date: 01/03/2023
+
+Bootcamp Python
+Johns Hopkins University
+Intersession 2023
+
+Project 1B: Decrypting encrypted message using reverse alphabet.
 """
 
