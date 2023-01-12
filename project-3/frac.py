fd5a521d21d7b13011e21dc32fb7e6a441c75662
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-11 23:34:17 -0500

    added project 3 and 4 folder and project 3 starter files

---
+++ project-3/frac.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 23:32:06 2023
+
+@author: patrickkariuki
+"""
+
---
+++ project-3/mall_buyers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 23:31:15 2023
+
+@author: patrickkariuki
+"""
+
