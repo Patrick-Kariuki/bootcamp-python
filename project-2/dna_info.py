2310ae971e76f5ff373b1bddad300d410d88b77c
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-16 23:33:43 -0500

    decode_sequence function done

--- project-2/dna_info.py
+++ project-2/dna_info.py
@@ -43,8 +43,9 @@ def encode_sequence(input_string):
             # Add the encoded DNA base to the DNA sequence
             dna_sequence += dna_encoding[bit_pair]
             
-    # Return the decoded dna_sequence
+    # Return the encoded dna_sequence
     return dna_sequence
+
 
 def decode_sequence(dna_input):
     """
@@ -60,43 +61,35 @@ def decode_sequence(dna_input):
     decoded_string : English text string decoded from dna_input
         
     """
-
-    # Dictionary to hold the DNA base - binary pair scheme
-    #dna_decoding = {"A": "00", "T": "01", "C": "10", "G": "11"}
-    # Initialize empty string to store decoded string to be returned
+    # Dictionary to hold DNA base - binary pair scheme
+    dna_decoding = {"A": "00", "T": "01", "C": "10", "G": "11"}
+    # Initialize empty string to store decoded English text message
+    # This string will be returned by the function
     decoded_string = ""
+    # Initialize empty string to hold binary representation of dna_input
+    binary_form = ""
     
-    # Iterate though each base in the dna input in steps of 4
-    # Because each Englist text character was converted to 8 bit to yield
-    # 4 DNA bases for its encoded message
-    for i in range(0, len(dna_input), 4):
-        # DNA bases representing a character in English text
-        char_dna_bases = dna_input[i: i + 4]
-        
-        # Iterate through each base in the character DNA bases
-        for base in char_dna_bases:
-            # Empty string to hold the binary form of the bases
-            binary_form = ""
-            # Map the base to binary and add to binary_form string
-            if base == "A":
-                binary_form += "00"
-            elif base == "T":
-                binary_form += "01"
-            elif base == "C":
-                binary_form += "10"
-            else:
-                binary_form += "11"
-        print(binary_form)
-        
-        # Convert the base to binary pair form and then to ascii value
-        ascii_value = int(binary_form, 2)
-        # Convert ascii value to English text and add to decoded string
+    # Iterate through bases in dna_input and get its decoded binary form
+    for base in dna_input:
+        binary_form += dna_decoding[base]
+    # Iterate through the binary form of dna_input in steps of 8
+    # Because 8 bits are used to represent ascii value of a character
+    for i in range(0, len(binary_form), 8):
+        # Gets the current 8 bits, convert to ascii and then character
+        char_bit = binary_form[i: i + 8]
+        ascii_value = int(char_bit, 2)
+        # English text character added to decoded string
         decoded_string += chr(ascii_value)
-        
-    # Return decoded sequence
+    
+    # Return the decoded English text
     return decoded_string
+
+
+def encrypt_decrypt(input_string, key = "CAT"):
+    return
 
 
 if __name__ == "__main__":
     print(encode_sequence("Frieza"))
     print(decode_sequence("TATCTGACTCCTTCTTTGCCTCAT"))
+    #print(encrypt_decrypt("TAAT"))
