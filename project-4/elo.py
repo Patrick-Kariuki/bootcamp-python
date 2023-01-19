b90c10c00604da77daac07dfc851cda19d35ddfc
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-18 22:26:24 -0500

    added pproject-4 starter python code file

---
+++ project-4/elo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Patrick Kariuki
+@JHED: pkariuk1
+Date: 01/15/2023
+
+Bootcamp Python
+Johns Hopkins University
+Intersession 2023
+
+Project 4
+"""
+
