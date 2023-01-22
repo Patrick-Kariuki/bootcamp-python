cbbc9ca53bd3d6630dd6d1e622711c2800f61a83
Patrick Kariuki <patmangu2018@gmail.com>
2023-01-22 12:27:43 -0500

    complete code for module 3

--- project-3/mall_buyers.py
+++ project-3/mall_buyers.py
@@ -13,6 +13,40 @@ Project 3
 """
 # Import the frac class
 import frac
+# Import random module
+import random
+
+
+class Node:
+    
+    """ Node class corresponding to each of the buildings in mall """
+    
+    def __init__(self, ID, connected_nodes, minimum_price, fractional_price):
+        """
+        Function: __init__: Initialize an instance of Node class
+
+        Parameters
+        ----------
+        ID : String to uniquely identify each building
+        connected_nodes : List of Node IDs to which this Node has outgoing 
+            edges
+        minimum_price : Fraction representing the minimum accepted price 
+            to make a purchase
+        fractional_price : Fraction representing the fraction of buyer's 
+            budget to use to buy if they can make the purchase
+
+        Returns
+        -------
+        None.
+
+        """
+        self.id = ID
+        self.connected_nodes = connected_nodes
+        self.minimum_price = minimum_price
+        self.fractional_price = fractional_price
+        # The amount collected in each building after a purchase
+        self.revenue = 0
+
 
 class Buyer:
     
@@ -36,4 +70,3 @@ class Buyer:
         """
         self.ID = current_node_id
         self.remaining_budget = remaining_budget
-        
